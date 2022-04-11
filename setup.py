482a9d8265b848575961e57345863cbe522412f3
AngryMane <regulationdango@gmail.com>
2022-04-11 16:02:27 +0900

    issue_#16 fix setup.py classifier and long_descriptions
    Signed-off-by: AngryMane <regulationdango@gmail.com>

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
 from setuptools import setup, find_packages
 from subprocess import run, CompletedProcess
+from os import path
 
-try:
-    with open('README.md') as f:
-        readme = f.read()
-except IOError:
-    readme = ''
-
+def read(fname):
+    return open(path.join(path.dirname(__file__), fname)).read()
 
 setup(
     name='mkdocs-glossary_plugin',
     version='0.1.0',
     description='This plugin automatically turns specific words into links to specific pages.',
-    long_description=readme,
+    long_description_content_type="text/markdown",
+    long_description=read("README.md"),
     keywords='mkdocs',
     url='https://github.com/AngryMane/mkdocs-glossary-plugin',
     author='AngryMane',
@@ -32,9 +30,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.7'
-        'Programming Language :: Python :: 3.8'
-        'Programming Language :: Python :: 3.9'
     ],
     packages=find_packages(),
     entry_points={
